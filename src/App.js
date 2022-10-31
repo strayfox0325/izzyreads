333122242dbe2a32ae09d6220ef339416115e3a7
Isidora Lazic <isidora.lazic@yahoo.com>
2022-11-01 00:02:42 +0100

    Completed registration with Laravel Sanctum

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import MasterLayout from "./layouts/admin/MasterLayout";
 import Home from "./components/frontend/Home";
 import Register from "./components/frontend/auth/Register";
 import Login from "./components/frontend/auth/Login";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://127.0.0.1:8000";
+axios.defaults.headers.post['Accept'] = 'application/json';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.defaults.withCredentials = true;
 
 function App() {
     return (
--- src/components/frontend/auth/Register.js
+++ src/components/frontend/auth/Register.js
@@ -1,11 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 import Navbar from "../../../layouts/frontend/Navbar";
-
+import axios from "axios";
+import swal from "sweetalert";
+import {useHistory} from "react-router-dom";
 function Register() {
+
+    const history = useHistory();
+
+    const [registerInput, setRegister] = useState({
+        name: '',
+        email: '',
+        password: '',
+        error_list: [],
+    });
+
+    const handleInput = (e) => {
+        e.persist();
+        setRegister({...registerInput,[e.target.name]: e.target.value});
+    }
+
+    const registerSubmit = (e) => {
+        e.preventDefault();
+        const data = {
+            name: registerInput.name,
+            email: registerInput.email,
+            password: registerInput.password,
+        }
+
+       // axios.get('/sanctum/csrf-cookie').then(response => {
+            axios.post(`/api/register`, data).then(res => {
+                if(res.data.status === 200) {
+                    localStorage.setItem('auth_token', res.data.token);
+                    localStorage.setItem('auth_name', res.data.username);
+                    swal("Success", res.data.message, "success");
+                    history.push('/');
+                } else {
+                    setRegister({...registerInput , error_list: res.data.validation_errors});
+                }
+            });
+       // });
+    }
+
+
+
     return (
         <div>
             <Navbar/>
-
             <div className="container py-5">
                 <div className="row justify-content-center">
                     <div className="col-md-6">
@@ -13,22 +53,21 @@ function Register() {
                             <div className="card-header">
                                 <h4>Register</h4>
                                 <div className="card-body">
-                                    <form>
+                                    <form onSubmit={registerSubmit}>
                                         <div className="form-group mb-3">
                                             <label>Full Name</label>
-                                            <input type="" name="name" className="form-control"/>
+                                            <input type="" name="name" onChange={handleInput} value={registerInput.name} className="form-control"/>
+                                            <span>{registerInput.error_list.name}</span>
                                         </div>
                                         <div className="form-group mb-3">
                                             <label>Email</label>
-                                            <input type="" name="email" className="form-control"/>
+                                            <input type="" name="email" onChange={handleInput} value={registerInput.email} className="form-control"/>
+                                            <span>{registerInput.error_list.email}</span>
                                         </div>
                                         <div className="form-group mb-3">
                                             <label>Password</label>
-                                            <input type="" name="password" className="form-control"/>
-                                        </div>
-                                        <div className="form-group mb-3">
-                                            <label>Confirm Password</label>
-                                            <input type="" name="confirm-password" className="form-control"/>
+                                            <input type="" name="password" onChange={handleInput} value={registerInput.password} className="form-control"/>
+                                            <span>{registerInput.error_list.password}</span>
                                         </div>
                                         <div className="form-group mb-3">
                                             <button type="submit" className="btn btn-primary" >Register</button>
@@ -39,9 +78,7 @@ function Register() {
                         </div>
                     </div>
                 </div>
-
             </div>
-
         </div>
     );
 }
