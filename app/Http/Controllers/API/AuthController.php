af2f35ab0691e9e71f9f96ea5ae3e0b5c271c79a
Isidora Lazic <isidora.lazic@yahoo.com>
2022-11-01 12:42:37 +0100

    Implemented logout

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -69,4 +69,11 @@ class AuthController extends Controller
         }
     }
 
+    public function logout() {
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            'status'=>200,
+            'message'=>'Logged Out Successfully',
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,9 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
 // Middleware
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')-> group(function() {
+    Route::post('logout', [AuthController::class, 'logout']);
+});
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
